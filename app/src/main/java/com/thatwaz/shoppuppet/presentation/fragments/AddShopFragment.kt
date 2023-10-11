a0bfee2ea553b6f1269e9f3c6049bdab501008dd
thatWaz <wazowski0425@gmail.com>
2023-10-11 01:24:32 -0400

    implement icon u.i. in add shop fragment

--- app/src/main/java/com/thatwaz/shoppuppet/presentation/fragments/AddShopFragment.kt
+++ app/src/main/java/com/thatwaz/shoppuppet/presentation/fragments/AddShopFragment.kt
@@ -5,26 +5,51 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.thatwaz.shoppuppet.R
+import com.thatwaz.shoppuppet.databinding.FragmentAddShopBinding
 
 
 class AddShopFragment : Fragment() {
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-
-        }
-    }
+    // Declare your binding variable
+    private var _binding: FragmentAddShopBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_shop, container, false)
+        // Initialize the binding object
+        _binding = FragmentAddShopBinding.inflate(inflater, container, false)
+
+        // Initially set your CardView as gone
+        binding.cvChooseIcon.visibility = View.GONE
+
+        binding.ibAlphabet.setOnClickListener {
+            val dialog = CustomIconDialogFragment()
+            dialog.show(childFragmentManager, "CustomIconDialog")
+        }
+
+
+        // Set the click listener for the next button
+        binding.btnShopName.setOnClickListener {
+            // Disable the EditText
+            binding.etShopName.isEnabled = false
+            binding.btnShopIcon.visibility = View.VISIBLE
+
+            // Dim the EditText
+            binding.etShopName.alpha = 0.5f
+            binding.btnShopName.alpha = 0.5f
+            // You might want to add some validation logic for the EditText here
+
+            // Make the CardView visible
+            binding.cvChooseIcon.visibility = View.VISIBLE
+        }
+
+        return binding.root
     }
 
-
+    override fun onDestroyView() {
+        super.onDestroyView()
+        // Avoid memory leak
+        _binding = null
+    }
 }
---
+++ app/src/main/java/com/thatwaz/shoppuppet/presentation/fragments/CustomIconDialogFragment.kt
@@ -0,0 +1,45 @@
+package com.thatwaz.shoppuppet.presentation.fragments
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.os.Bundle
+import android.widget.EditText
+import androidx.fragment.app.DialogFragment
+import com.thatwaz.shoppuppet.R
+
+class CustomIconDialogFragment : DialogFragment() {
+
+    interface CustomIconDialogListener {
+        fun onIconTextEntered(text: String)
+    }
+
+    private var listener: CustomIconDialogListener? = null
+
+//    override fun onAttach(context: Context) {
+//        super.onAttach(context)
+//        try {
+//            listener = parentFragment as CustomIconDialogListener
+//        } catch (e: ClassCastException) {
+//            throw ClassCastException("$context must implement CustomIconDialogListener")
+//        }
+//    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return activity?.let {
+            val builder = AlertDialog.Builder(it)
+
+            val inflater = requireActivity().layoutInflater
+            val view = inflater.inflate(R.layout.dialog_custom_icon, null)
+
+            builder.setView(view)
+                .setTitle("Custom Shop Icon")
+                .setPositiveButton("OK") { _, _ ->
+                    val editText = view.findViewById<EditText>(R.id.et_custom_icon)
+                    listener?.onIconTextEntered(editText.text.toString())
+                }
+                .setNegativeButton("Cancel") { _, _ -> dismiss() }
+
+            builder.create()
+        } ?: throw IllegalStateException("Activity cannot be null")
+    }
+}
