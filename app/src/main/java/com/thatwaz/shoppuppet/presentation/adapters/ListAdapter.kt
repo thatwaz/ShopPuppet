ab15788811305752e811b79fd461963fc3b20bf4
thatWaz <wazowski0425@gmail.com>
2023-12-28 02:11:03 -0500

    implement chip background colors to match assigned shop colors

--- app/src/main/java/com/thatwaz/shoppuppet/presentation/adapters/ListAdapter.kt
+++ app/src/main/java/com/thatwaz/shoppuppet/presentation/adapters/ListAdapter.kt
@@ -1,7 +1,6 @@
 package com.thatwaz.shoppuppet.presentation.adapters
 
 import android.content.res.ColorStateList
-import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -26,37 +25,76 @@ class ListAdapter(private val itemClickListener: ItemClickListener
         RecyclerView.ViewHolder(binding.root) {
 
 
-
-
         fun bind(itemUiModel: ItemUiModel) {
             binding.tvItemName.text = itemUiModel.itemName
 
-             if (itemUiModel.isPriorityItem) {
+            if (itemUiModel.isPriorityItem) {
                 binding.imgStar.visibility = View.VISIBLE
             } else {
-               binding.imgStar.visibility = View.INVISIBLE
+                binding.imgStar.visibility = View.INVISIBLE
             }
 
             // Clear any existing chips
             binding.chipGroupShops.removeAllViews()
 
             // Populate chips based on the shops
-            itemUiModel.shopNames.forEach { shopName ->
+            itemUiModel.shopNames.forEach { shop ->
                 val chip = Chip(itemView.context)
-                chip.setChipBackgroundColorResource(R.color.off_white)
-                val strokeColor =
-                    ContextCompat.getColor(itemView.context, R.color.shop_blue)
-                chip.chipStrokeColor = ColorStateList.valueOf(strokeColor)
-                chip.chipStrokeWidth = TypedValue.applyDimension(
-                    TypedValue.COMPLEX_UNIT_DIP,
-                    1f,
-                    itemView.resources.displayMetrics
-                )
-                chip.text = shopName.name
+
+                // Get the color resource ID from the shop's color name
+                val colorResId = itemView.context.resources.getIdentifier(
+                    shop.colorResName, "color", itemView.context.packageName)
+                chip.setTextColor(ContextCompat.getColor(itemView.context, R.color.white))
+                // Use the shop's color for the chip
+                if (colorResId != 0) {
+                    val chipColor = ContextCompat.getColor(itemView.context, colorResId)
+                    chip.chipBackgroundColor = ColorStateList.valueOf(chipColor)
+                } else {
+                    // Fallback color if resource not found
+                    chip.chipBackgroundColor = ColorStateList.valueOf(
+                        ContextCompat.getColor(itemView.context, R.color.black))
+                }
+
+                // Set other chip properties
+                chip.text = shop.name
                 chip.isClickable = false
                 chip.isCheckable = false
+
+                // Add chip to the group
                 binding.chipGroupShops.addView(chip)
             }
+
+
+
+//        fun bind(itemUiModel: ItemUiModel) {
+//            binding.tvItemName.text = itemUiModel.itemName
+//
+//             if (itemUiModel.isPriorityItem) {
+//                binding.imgStar.visibility = View.VISIBLE
+//            } else {
+//               binding.imgStar.visibility = View.INVISIBLE
+//            }
+//
+//            // Clear any existing chips
+//            binding.chipGroupShops.removeAllViews()
+//
+//            // Populate chips based on the shops
+//            itemUiModel.shopNames.forEach { shopName ->
+//                val chip = Chip(itemView.context)
+//                chip.setChipBackgroundColorResource(R.color.off_white)
+//                val strokeColor =
+//                    ContextCompat.getColor(itemView.context, R.color.shop_blue)
+//                chip.chipStrokeColor = ColorStateList.valueOf(strokeColor)
+//                chip.chipStrokeWidth = TypedValue.applyDimension(
+//                    TypedValue.COMPLEX_UNIT_DIP,
+//                    1f,
+//                    itemView.resources.displayMetrics
+//                )
+//                chip.text = shopName.name
+//                chip.isClickable = false
+//                chip.isCheckable = false
+//                binding.chipGroupShops.addView(chip)
+//            }
 
             binding.imgDropDown.setOnClickListener {
                 if (binding.chipGroupShops.visibility == View.VISIBLE) {
@@ -98,13 +136,13 @@ class ListAdapter(private val itemClickListener: ItemClickListener
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShoppingViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListAdapter.ShoppingViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         val binding = ShoppingItemBinding.inflate(layoutInflater, parent, false)
-        return ShoppingViewHolder(binding, itemClickListener)
+        return ListAdapter.ShoppingViewHolder(binding, itemClickListener)
     }
 
-    override fun onBindViewHolder(holder: ShoppingViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ListAdapter.ShoppingViewHolder, position: Int) {
         val item = getItem(position)
         holder.bind(item)
     }
