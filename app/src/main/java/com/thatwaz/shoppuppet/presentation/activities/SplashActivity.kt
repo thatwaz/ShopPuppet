293358398792d496bb2867eae77e73b7a63e12d1
thatWaz <wazowski0425@gmail.com>
2024-04-04 02:24:28 -0400

    reimplement splash screen and launcher icon.

---
+++ app/src/main/java/com/thatwaz/shoppuppet/presentation/activities/SplashActivity.kt
@@ -0,0 +1,31 @@
+package com.thatwaz.shoppuppet.presentation.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.thatwaz.shoppuppet.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // Start fade-in animation
+        val imageView = findViewById<ImageView>(R.id.splash_image)
+        val fadeInAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+        imageView.startAnimation(fadeInAnimation)
+
+        // Delay for the splash screen
+        Handler(Looper.getMainLooper()).postDelayed({
+            // Start the main activity after the delay
+            val mainIntent = Intent(this, MainActivity::class.java)
+            startActivity(mainIntent)
+            finish()
+        }, 2000) // 2000 ms for 2 seconds delay
+
+    }
+}
